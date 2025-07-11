96dffa0227c7ae20d0af3c11dccc9c3bd684a6cf
Karadev <elik@example.com>
2025-07-12 00:51:17 +0500

    add fix mobile vers

--- home/forms.py
+++ home/forms.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 class CalculatorForm(forms.Form):
     target = forms.CharField(
         label='Размер мишени',
+        help_text='В метрах',
         widget=forms.TextInput(attrs={'class': 'small-input'})
     )
     dist = forms.CharField(
         label='Дистанция до мишени',
+        help_text='от 100м до 1500м',
         widget=forms.TextInput(attrs={'class': 'small-input'})
     )
 
